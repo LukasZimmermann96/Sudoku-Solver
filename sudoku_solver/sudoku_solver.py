5d4d77b8ad03ac2ea30ebd7adaba236d47c7b25e
Lukas Zimmermann <z.lukas.1996@gmail.com>
2023-08-11 14:43:14 +0200

    create empty functions

---
+++ sudoku_solver/sudoku_solver.py
@@ -0,0 +1,22 @@
+board = None
+ALL_OPTIONS = [x + 1 for x in range(9)]
+possibleOptionBoard = None
+
+
+def init_board(input):
+  return None
+
+def init_possible_options_board():
+  return None
+
+def check_board_is_solved(Board):
+  return None
+
+def get_options_for_cell(length_X, height_Y, board, Options):
+  return None
+
+def solve():
+  return None
+
+def print_board(Board):
+  return None
